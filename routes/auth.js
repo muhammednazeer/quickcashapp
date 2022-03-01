c0de0b4b7cd46c8d14902f751b7a41c35e1982a2
muhammednazeer <nazeer4u2c@gmail.com>
2022-03-01 18:36:11 +0100

    modified the auth routes by moving other routes to a separate files

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,28 @@
-const router = require('express').Router();
+const authRoute = require('express').Router();
+const User = require('../model/User');
+const { loginValidation } = require('../middleware/validation');
+const bcrypt = require('bcryptjs');
 
-router.post('/register', (req, res) => {
-    res.send("Register");
+//Login Route
+authRoute.post('/login', async (req, res) => {
+    
+    //Validate before login  User
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the email exist
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Email is Incorrect');
+    
+
+    //PASSWORD IS CORRECT
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Password is Incorrect');
+
+    //Create a session token
+    req.session.userId = user._id
+    res.redirect('/dashboard');
+
 });
 
-router.poster('/login', (req, res) => {
-    res.send("Login");
-})
-modules.export = router;
+module.exports = authRoute;
