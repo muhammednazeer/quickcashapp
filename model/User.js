736869e4db284fd058495b5d7dfcc4fcf87b4d2c
muhammednazeer <nazeer4u2c@gmail.com>
2022-03-01 18:37:03 +0100

    modify the app User model

--- model/User.js
+++ model/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const userSchema = mongoose.Schema({
-    firsName: {
+    firstName: {
         type: String,
         required: true,
         min: 3,
@@ -19,12 +20,13 @@ const userSchema = mongoose.Schema({
         min: 6,
         max: 255
     },
-    pasword: {
+    password: {
         type: String,
         required: true,
         min: 6,
         max: 1024
-    }
+    },
+   
 });
 
 module.exports = mongoose.model('User', userSchema);
