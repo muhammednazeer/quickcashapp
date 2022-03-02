eb36044d073ad46aae72bd2b0c09a138b878ca4f
muhammednazeer <nazeer4u2c@gmail.com>
2022-03-02 16:37:09 +0100

    Update the User model schema

--- model/User.js
+++ model/User.js
@@ -26,7 +26,57 @@ const userSchema = mongoose.Schema({
         min: 6,
         max: 1024
     },
-   
+    account: {
+        usd: {
+            type: Number,
+            min: 0,
+            required: true
+        },
+        eur: {
+            type: Number,
+            min: 0,
+            required: true
+        },
+        ngn: {
+            type: Number,
+            min: 0,
+            required: true
+        },
+
+    },
+    transactions:([{
+        createdAt: {
+            type: Date,
+            default: moment().utc().format('Y-M-D H:M:S')
+        },
+        updatedAt: {
+            type: Date,
+            default: moment().utc().format('Y-M-D H:M:S')
+        },
+        transactionId: {
+            type: Number,
+            default: 0
+
+        },
+        from: {
+            type: String,
+            required: true,
+        },
+        to: {
+            type: String,
+            required: true,
+        },
+        value: {
+            type: Number,
+            required: true,
+            min: 1,
+        },
+        currency: {
+            type: String,
+            required: true,
+        }
+
+    }])
 });
 
 module.exports = mongoose.model('User', userSchema);
